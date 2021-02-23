e7bc3d07421889a0fe51b141cdd10e2ea8e54cf7
Venkatesan-dharmaraj <venkatesandmca@gmail.com>
2021-02-23 21:11:18 +0530

    UI changes committed

--- klm-frontend/src/app/flight-search/flight-search.component.ts
+++ klm-frontend/src/app/flight-search/flight-search.component.ts
@@ -23,13 +23,55 @@ export class FlightSearchComponent implements OnInit {
     {value: 'tacos-2', viewValue: 'Tacos'}
   ];
   selectedCar:any ;
-
+  selectedDepartment:any;
+  selectedArrival:any;
+  classType:any;
+  classes =[
+    {
+      id:'EC',value:'Economy Class'
+    },
+    {
+      id:'PC',value:'Premium Class'
+    },
+    {
+      id:'BC',value:'Business Class'
+    }
+  ]
   cars = [
       { id: 1, name: 'Volvo' },
       { id: 2, name: 'Saab' },
       { id: 3, name: 'Opel' },
       { id: 4, name: 'Audi' },
   ];
+
+  trip = [
+    { id: 0, name: 'Round Trip' },
+    { id: 1, name: 'one-way' },
+    { id: 2, name: 'Multi-city' }
+  ];
+  departList=[
+    {name: 'Afghanistan', code: 'AF'}, 
+    {name: 'Åland Islands', code: 'AX'}, 
+    {name: 'Albania', code: 'AL'}, 
+    {name: 'Algeria', code: 'DZ'}, 
+    {name: 'American Samoa', code: 'AS'}, 
+    {name: 'AndorrA', code: 'AD'}, 
+    {name: 'Angola', code: 'AO'}, 
+    {name: 'Anguilla', code: 'AI'}, 
+    {name: 'Antarctica', code: 'AQ'}, 
+    {name: 'Antigua and Barbuda', code: 'AG'}, 
+    {name: 'Argentina', code: 'AR'}, 
+    {name: 'Armenia', code: 'AM'}, 
+    {name: 'Aruba', code: 'AW'}, 
+    {name: 'Australia', code: 'AU'}, 
+    {name: 'Austria', code: 'AT'}, 
+    {name: 'Azerbaijan', code: 'AZ'}, 
+    {name: 'Bahamas', code: 'BS'}, 
+    {name: 'Bahrain', code: 'BH'}, 
+    {name: 'Bangladesh', code: 'BD'}, 
+    {name: 'Barbados', code: 'BB'}, 
+    {name: 'Belarus', code: 'BY'}
+  ]
 
 
 }
